48e7f9d56acbee6dbae0221a81536ef8b3216a05
alexgrayharris <alex.gray.harris@gmail.com>
2017-07-16 21:40:17 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $('#left-info-slider').hover(function() {
+    $('#left-info').toggle(600);
+  });
+
+  $('#right-info-slider').hover(function() {
+    $('#right-info').toggle(600);
+  });
+
+
+
+})
